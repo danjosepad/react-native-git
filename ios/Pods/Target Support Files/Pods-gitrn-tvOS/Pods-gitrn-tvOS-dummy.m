e853d680e371e28cbb41085615b2035e85758cdb
danjosepad <danielpadilhapessoal@outlook.com>
2020-03-12 16:43:38 -0300

    feat: screen, axios and styled added

---
+++ ios/Pods/Target Support Files/Pods-gitrn-tvOS/Pods-gitrn-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gitrn_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_gitrn_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-gitrn/Pods-gitrn-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gitrn : NSObject
+@end
+@implementation PodsDummy_Pods_gitrn
+@end
