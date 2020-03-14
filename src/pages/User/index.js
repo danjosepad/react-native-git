e23504aa7faeef009f920560cc6083f96477b57d
danjosepad <danielpadilhapessoal@outlook.com>
2020-03-14 07:47:55 -0300

    styled: User css added

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   constructor() {
@@ -23,9 +34,34 @@ export default class User extends Component {
   }
 
   render() {
+    const { route } = this.props;
+    const { user } = route.params;
+
     const { stars } = this.state;
 
-    return <View />;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={star => String(star.id)}
+          renderItem={({ item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+              <Info>
+                <Title>{item.name}</Title>
+                <Author>{item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    );
   }
 }
 
